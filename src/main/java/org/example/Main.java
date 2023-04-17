f7a660c5acea43d13536d0582df623afc48d1b6a
dolvenia <dolvenia@gmail.com>
2023-04-17 14:10:20 -0700

    added a for statement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,40 @@
 package org.example;
 
 import org.example.models.Book;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        input.nextLine();
+
+        int Num = 2;
+        Book[] book = new Book[Num];
+
+        String BookName;
+        
+        for (int i = 0; i < Num; i++) {
+
+            BookName = input.nextLine();
+            book[i] = new Book(BookName);
+
+        }
+        for (int i = 0; i < Num; i++) {
+            book[i].show();
+
+
+       /* System.out.println("Hello world!");
 
         Book book1 = new Book(123456, "BookName", 15.00);
         Book book2 = new Book();
 
         System.out.println(book1.getBookName());
+        */
 
-
+        }
     }
 }
+
 
 
 
