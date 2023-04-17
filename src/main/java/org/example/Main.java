a1fb23e3f646d70fbf7c0e0f039d090fded2a9dd
dolvenia <dolvenia@gmail.com>
2023-04-17 07:40:21 -0700

    removed new line

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println("New Line");
-
         Book book1 = new Book(123456, "BookName", 15.00);
         Book book2 = new Book();
 
         System.out.println(book1.getBookName());
+
+
     }
 }
+
+
+
+
