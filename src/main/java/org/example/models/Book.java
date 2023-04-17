065b1e10ff08a521d0e27855d96c6564f17a3eca
dolvenia <dolvenia@gmail.com>
2023-04-17 09:15:10 -0700

    added book method

--- src/main/java/org/example/models/Book.java
+++ src/main/java/org/example/models/Book.java
@@ -1,40 +1,70 @@
 package org.example.models;
 
 public class Book {
-    private int bookId;
-    private String bookName;
+    private int BookId;
+    private String BookName;
     private double price;
 
-    public Book(int bookId, String bookName, double price) {
-        this.bookId = bookId;
-        this.bookName = bookName;
+    private Boolean isBorrowed;
+
+    public Book(int BookId, String BookName, double price) {
+        this.BookId = BookId;
+        this.BookName = BookName;
         this.price = price;
+        this.isBorrowed = false;
     }
 
     public Book() {
     }
 
     public int getBookId() {
-        return bookId;
+
+        return BookId;
     }
 
-    public void setBookId(int bookId) {
-        this.bookId = bookId;
+    public void setBookId(int BookId) {
+
+        this.BookId = BookId;
     }
 
     public String getBookName() {
-        return bookName;
+
+        return BookName;
     }
 
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
+    public void setBookName(String BookName) {
+
+        this.BookName = BookName;
     }
 
     public double getPrice() {
+
         return price;
     }
 
     public void setPrice(double price) {
         this.price = price;
     }
+
+    public void BorrowBook(){
+        isBorrowed = true;
+    }
+     public void ReturnBook(){
+        isBorrowed = false;
+     }
+     public Boolean isBorrowed(){
+        return isBorrowed;
+     }
+
+    public void show() {
+        System.out.println("Book ID: " + BookId);
+        System.out.println("Book Name: " + BookName);
+        System.out.print("Status: ");
+        if (isBorrowed)
+            System.out.println("Not Available");
+        else
+            System.out.println("Available");
+        System.out.println();
+    }
 }
+
